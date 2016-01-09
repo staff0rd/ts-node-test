2d5b82addbbedea0dc2c3b2622e27b5961458cdd
Stafford Williams <stafford.williams@gmail.com>
2016-01-09 18:44:35 +1100

    example

---
+++ MyClass.ts
@@ -0,0 +1,8 @@
+class MyClass {
+    x: number;
+    y: number;
+    constructor(x: number, y :number) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ MyScript.ts
@@ -0,0 +1,3 @@
+/// <reference path="MyClass.ts" />
+var myClass: MyClass = new MyClass(2,3);
+console.log(myClass.x, myClass.y);
